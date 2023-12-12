f4f2e905400f8b6389a1abab4f4bf4ece0a5dc65
Anna <anjanadvorna@gmail.com>
2023-12-12 22:38:05 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 // import { persistStore, persistReducer } from 'redux-persist';
 // import storage from 'redux-persist/lib/storage';
 import { filterSlice } from './filterSlice';
-import { contactsReduser, contactsSlice} from './contactsSlice';
+import { contactsSlice} from './contactsSlice';
 
 // const persistConfig = {
 //   key: 'root',
